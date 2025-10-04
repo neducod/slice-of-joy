e3d667224ad84a76a49127c1ce10cc5266d3a45b
neducod <nedunony16@gmail.com>
2025-10-05 00:14:23 +0100

    trying to fix hero

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", () => {
     const navbar = document.getElementById("navbar");
-    if (window.scrollY > 100) {
+    if (window.scrollY > 190) {
       navbar.classList.add("scrolled");
     } else {
       navbar.classList.remove("scrolled");
